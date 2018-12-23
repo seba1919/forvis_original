4d4d2604168ed4fc1f0d675146930ebcea67bfe2
klusm <Mateusz.Kluska@pega.com>
2018-12-23 14:46:46 +0000

    Allow localhost host

--- formulavis/settings.py
+++ formulavis/settings.py
@@ -14,7 +14,7 @@ import os
 
 from kombu import Exchange, Queue
 
-ALLOWED_HOSTS = ["forvis.agh.edu.pl"]
+ALLOWED_HOSTS = ["forvis.agh.edu.pl", "localhost", "127.0.0.1"]
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
