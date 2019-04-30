8deb9bc5bfb6f7b94bcab9119d57136a6af1bb14
Magda <magdtrag@gmail.com>
2019-04-30 15:30:11 +0200

    add custom management command for users removing

---
+++ profiles/management/commands/remove_users.py
@@ -0,0 +1,17 @@
+from django.core.management.base import BaseCommand, CommandError
+
+from django.contrib.auth.models import User
+from datetime import datetime
+from django.utils import timezone
+from datetime import timedelta
+
+class Command(BaseCommand):
+	help= ''
+
+	def handle(self, *args, **kwargs):
+    	users = User.objects.all()
+		now = datetime.now(tz=timezone.utc)
+
+		for user in users:
+    		if now - user.last_login > timedelta(days=6*30):
+       		user.delete()
